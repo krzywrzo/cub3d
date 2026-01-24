2f1eb831c6b19ba0b81220785d59afcf0517a73c
krzywrzo <k.wrzosek03@gmail.com>
2026-01-24 16:46:52 +0100

    minilibx

--- src/main.c
+++ src/main.c
@@ -6,16 +6,23 @@
 /*   By: kwrzosek <kwrzosek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 15:43:50 by kwrzosek          #+#    #+#             */
-/*   Updated: 2026/01/24 15:48:43 by kwrzosek         ###   ########.fr       */
+/*   Updated: 2026/01/24 16:44:43 by kwrzosek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/cub3d.h"
+#include "../includes/cub3d.h"
 
 int main (int argc, char **argv)
 {
-	if (argc != 2)
+	char	*map_path;
+
+	if (argv != 2)
 	{
 		return (1);
+	}
+	ft_strlcpy(map_path, argv[1], ft_strlen(argv[1]));
+	if (map_parse(map_path) == 1)
+	{
+		handle_error();	//	TODO
 	}
 }
