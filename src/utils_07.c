4e5705b2878efd12744cb714b2ea38d213df905b
krzywrzo <74268723+krzywrzo@users.noreply.github.com>
2026-07-20 17:39:14 +0200

    FIX: wrong parsing of color (lack of minus validation)

--- src/utils_07.c
+++ src/utils_07.c
@@ -6,11 +6,25 @@
 /*   By: kwrzosek <kwrzosek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 14:08:34 by kwrzosek          #+#    #+#             */
-/*   Updated: 2026/07/16 19:20:04 by kwrzosek         ###   ########.fr       */
+/*   Updated: 2026/07/20 17:37:31 by kwrzosek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+int	is_minus(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] != '\n')
+	{
+		if (line[i] == '-')
+			return (-1);
+		i++;
+	}
+	return (0);
+}
 
 int	check_fc_line(t_fc *fc, char *line)
 {
@@ -18,11 +32,15 @@ int	check_fc_line(t_fc *fc, char *line)
 	{
 		if (fc->f != NULL || check_line_garbage(line) == -1)
 			return (-1);
+		if (is_minus(line) == -1)
+			return (-1);
 		fc->f = ft_strdup(line);
 	}
 	else if (ft_strlcmp(line, "C ", 2) == 0 || ft_strlcmp(line, "C\t", 2) == 0)
 	{
 		if (fc->c != NULL || check_line_garbage(line) == -1)
+			return (-1);
+		if (is_minus(line) == -1)
 			return (-1);
 		fc->c = ft_strdup(line);
 	}
